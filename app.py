250a989f864afe7e9e96a616f1f0750dcd6bc247
tomas <tomas.cntras@gmail.com>
2025-06-26 14:53:55 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -7,38 +7,27 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def show_table():
+def home():
 
     df = pd.read_csv('denue_inegi_22_.csv', encoding='latin1')
     oxxos = df[df.raz_social == "CADENA COMERCIAL OXXO SA DE CV"]
-
-    agebs = len(oxxos.ageb.unique())
-    num_oxxos = len(oxxos)
 
     table_html = oxxos.to_html(classes='table table-striped table-bordered', index=False, max_rows=10)
 
-    return render_template('table.html', table=table_html, agebs=agebs, num_oxxos=num_oxxos)
+    return render_template('home.html', table=table_html)
 
-@app.route('/mapa_oxxos')
-def mapa_oxxos():
-    df = pd.read_csv('denue_inegi_22_.csv', encoding='latin1')
-    oxxos = df[df.raz_social == "CADENA COMERCIAL OXXO SA DE CV"]
+@app.route('/flujo')
+def flujo():
+    return render_template('flujo.html')
 
-    mapa = folium.Map(location=[oxxos['latitud'].mean(), oxxos['longitud'].mean()], zoom_start=13)
+@app.route('/auditadas')
+def auditadas():
+    return render_template('auditadas.html')
 
-    for _, row in oxxos.iterrows():
-        folium.CircleMarker(
-            location=[row['latitud'], row['longitud']],
-            popup=row['nom_estab'],
-            radius=5,
-            color='red',
-            fill=True,
-            fill_color='red'
-        ).add_to(mapa)
-    if not os.path.exists('static'):
-        os.makedirs('static')
-    mapa.save('static/mapa.html')
-    return render_template('mapa.html')
+@app.route('/atractores')
+def atractores():
+    return render_template('atractores.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
